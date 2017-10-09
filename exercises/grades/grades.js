a8b7bd57c30efc3b85e3ceaf64aaa748d6f6f961
Paul Ellis <tynesellis@gmail.com>
2017-10-08 19:33:58 -0500

    replaced sorting loop with dryer idea

--- exercises/grades/grades.js
+++ exercises/grades/grades.js
@@ -1,32 +1,32 @@
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
 const grades = {
-    "a": 0,
-    "b": 0,
-    "c": 0,
-    "d": 0,
-    "f": 0
+    "A": 0,
+    "B": 0,
+    "C": 0,
+    "D": 0,
+    "F": 0
 } 
 
-for (let i = 0; i < scores.length; i++) {
-    let benchmark = 90;//beginning value to compare with scores
-    let letters = ["a", "b", "c", "d", "f"] //for use in assigning values to keys in grades object - see if statement in next for loop
-    for (var l = 0; l < letters.length; l++) {
-        if (scores[i] > benchmark) {
-            grades[letters[l]] ++;
-            benchmark -= 10;
-            break
-        } else {benchmark -= 10;}
-    }//end for loop to compare scores to grades and assign to proper grades object key
+
+let benchmark = 90;//beginning value to compare with scores
+//for each grade category in grades object, loops through scores array and adds to each key value of grades
+for (let value in grades){
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] > benchmark && scores[i] <= (benchmark + 10) ) {
+            grades[value] ++;
+        }  
+    } benchmark -= 10;
 }
+
 
 console.log(grades)
 
 for (let key in grades) {
-    console.log(key.toUpperCase() + "'s : " + grades[key])
+    console.log(key + "'s : " + grades[key])
 }//Display grades by letter with corresponding number
 
-//Lowest score
-var lowestScore = 101;
+//Lowest score - loops through scores array and stores lowest score until a lower score is found or the loop completes
+let lowestScore = 101;
 for (var index = 0; index < scores.length; index++) {
     if (scores[index] < lowestScore) {
         lowestScore = scores[index];
@@ -35,8 +35,8 @@ for (var index = 0; index < scores.length; index++) {
 
 console.log("Lowest score was: " + lowestScore);
 
-//Highest score
-var highestScore = 0;
+//Highest score - same as above, but with highest score
+let highestScore = 0;
 for (var index = 0; index < scores.length; index++) {
     if (scores[index] > highestScore) {
         highestScore = scores[index];
@@ -51,28 +51,25 @@ let currentGradeCount = 0;
 let highestGradeCategory = [];
 
 
-//Finds the grade with the highest number of scores
+//Finds the value of the grade key with the highest number of scores
 for (let value in grades) {
     if (grades[value] > currentGradeCount) {
         currentGradeCount = grades[value];
     } 
 }
 
-
+//pushes the letter grade category with the highest number into array, as there might be a tie
 for (let value in grades) {
     if (grades[value] === currentGradeCount) {
-        highestGradeCategory.push(value.toUpperCase());
+        highestGradeCategory.push(value);
     }
 }
-
+//scenarios for one grade category having the highest or a tie
 if (highestGradeCategory.length === 1) {
     mostCommonGrade += highestGradeCategory[0] + "."
 } else {
     mostCommonGrade += "a tie between: " + highestGradeCategory.slice(0, (highestGradeCategory.length - 1)).join(", ") + " and " + highestGradeCategory.slice(highestGradeCategory.length - 1) + ".  Each grade category had " + currentGradeCount + " scores.";
 }
-
-
-
 
 console.log(mostCommonGrade)
 
