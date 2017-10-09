aa227c7cad9970064d4f7033c3e68299e7036e30
Paul Ellis <tynesellis@gmail.com>
2017-10-09 10:55:13 -0500

    cleaned up and sorted

---
+++ exercises/stringify/storage.js
@@ -0,0 +1,20 @@
+// const = {
+//     name:
+//     type:
+//     location:
+//     description:
+// }
+
+const gateleg = {
+        name: "Gateleg Table",
+        type: "furniture",
+        location: "Living Room",
+        description: "Antique gateleg table, given to me by my parents.  It has been in the family for at least three generations."
+    }
+
+const coffeeTable = {
+    name: "Coffee Table",
+    type: "furniture",
+    location: "Living Room",
+    description: "Coffee table I built after I broke my old one."
+}
