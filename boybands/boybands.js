fbf74315c0a68de4e9bb6e22b7b9398660d11fda
Paul Ellis <tynesellis@gmail.com>
2017-10-10 13:10:51 -0500

    Bye Bye Bye

---
+++ boybands/boybands.js
@@ -0,0 +1,27 @@
+let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+let arrayz = [bands, vegetables];
+
+// Get a reference to the appropriate DOM element for bands
+const bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+const veggieElement = document.getElementById("vegetables");
+let htmlRef = [bandElement, veggieElement];
+
+// Execute a for loop that will iterate over the arrays
+for (let loopTracker = 0; loopTracker < arrayz.length; loopTracker += 1) {
+    ref = htmlRef[loopTracker];
+    let currentArray = arrayz[loopTracker];
+    for (var i = 0; i < currentArray.length; i++) {
+          // Get a reference to the current item in the  array
+        let currentItem = currentArray[i];
+        let newID = currentItem.replace(/ /g, "");
+        ref.innerHTML += `
+        <p id="${newID}">${currentItem}</p>
+        `
+    }
+}
+
+let correction = document.getElementById("BoyzIIMen");
+correction.innerHTML += ` **Not actually a boy band**`
