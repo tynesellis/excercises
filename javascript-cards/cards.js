ffc93f23d9ba3c3b6a754688521155d197dac650
Paul Ellis <tynesellis@gmail.com>
2017-10-23 15:57:27 -0500

    pause to drive home

--- javascript-cards/cards.js
+++ javascript-cards/cards.js
@@ -1,4 +1,4 @@
-let newHTML = document.getElementById("newCardsArea");
+let newHTML = document.getElementById("newCardsArea");//target area to write new HTML
 
 // unique ID generator
 const idGenerator = function* () {
@@ -11,13 +11,19 @@ const idGenerator = function* () {
 
 const uniqueID = idGenerator();//instance of idGenerator
 
-document.getElementById("createButton").addEventListener("click", function(){
-    let newCardText = document.getElementById("initialText").value;
+document.getElementById("createButton").addEventListener("click", function(){//target the create button to trigger new DOM conent
+    let newCardText = document.getElementById("initialText").value;//get the text typed into the text area input
     newHTML.innerHTML += `
-        <div class="card" id="card_${uniqueID.next()}">
+        <div class="card" id="card_${uniqueID.next().value}">
             <section>${newCardText}</section>
-            <button id="button_${uniqueID.next()}">Delete</button>
+            <button id="deleteButton_${uniqueID.next().value}">Delete</button>
         </div>
     `
+
+});
+
+document.addEventListener("click", function(event){
+    console.log(event.target.id)
+
 
 })
