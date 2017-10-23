a330e186efb0f3f8538cf96ff1587bbea1bb09aa
Paul Ellis <tynesellis@gmail.com>
2017-10-23 18:05:47 -0500

    functioning and beautiful

--- javascript-cards/cards.js
+++ javascript-cards/cards.js
@@ -13,17 +13,20 @@ const uniqueID = idGenerator();//instance of idGenerator
 
 document.getElementById("createButton").addEventListener("click", function(){//target the create button to trigger new DOM conent
     let newCardText = document.getElementById("initialText").value;//get the text typed into the text area input
+    let idPlugIn = uniqueID.next().value;//get a unique id number to be added to the id's below
     newHTML.innerHTML += `
-        <div class="card" id="card_${uniqueID.next().value}">
-            <section>${newCardText}</section>
-            <button id="deleteButton_${uniqueID.next().value}">Delete</button>
+        <div class="card" id="card_${idPlugIn}">
+            <section class="cardText">${newCardText}</section>
+            <button class="byeBye" id="deleteButton_${idPlugIn}">Delete</button>
         </div>
     `
-
+    document.getElementById("initialText").value = "";//reset form field
 });
 
-document.addEventListener("click", function(event){
-    console.log(event.target.id)
-
-
+document.addEventListener("click", function(event){//listen for any click event 
+    if (event.target.id.split("_")[0] === "deleteButton") {//test to see if the click was a delete button
+    const identity = (event.target.id).split("_")[1];//extract number of button, as it will match the card container
+    const childCard = document.getElementById(`card_${identity}`);//select the card that contains the delete button
+    newHTML.removeChild(childCard);//remove that card by selecting it as the child of the main section with the id matching the button
+    }
 })
